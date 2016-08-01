de6eb6d159c3b035febe79140f78c7e40160f98e
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-08-01 10:25:06 +0200

    Initial Commit

---
+++ Where Am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by Ignacio Lasaosa Sáez on 1/8/16.
+//  Copyright © 2016 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
